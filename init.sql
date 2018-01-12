2e8788d034e169786140ada0b5fe9ed937d1bb4a
limingliu <limingliu09@163.com>
2018-01-12 14:41:53 +0800

    添加注释

--- init.sql
+++ init.sql
@@ -1,18 +1,18 @@
 create SCHEMA test;
 
 create table test.model_statistic (
-  score_name varchar(32),
-  period varchar(32),
-  score varchar(32),
-  counts int
+  score_name varchar(32), -- 评分模型名
+  period varchar(32), -- 账期
+  score varchar(32), -- 分值
+  counts int -- 统计值
 ) charset=utf8;
 
 create table test.feature_statistic (
-  score_name varchar(32),
-  period varchar(32),
-  feature_name varchar(32),
-  value varchar(32),
-  counts int
+  score_name varchar(32), -- 评分模型名
+  period varchar(32), -- 账期
+  feature_name varchar(32), -- 特征名
+  value varchar(32), -- 特征取值
+  counts int  -- 统计值
 ) charset=utf8;
 
 DROP TABLE test.model_statistic;
